f4e0c79619e1a56e27d914bd0c47670f1d14b585
Damien Pugin <damienpugin19@gmail.com>
2024-05-22 10:31:23 +0200

    add gunicorn and port 8000

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return jsonify(message="Hello, World!")
+@app.route('/')
+def hello():
+    return "Hello, Heroku!"
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
